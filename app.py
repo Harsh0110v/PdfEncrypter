35ac6282b8463ffd6200797dd474ef4210e83df2
Harsh Varma <harshvarma0110@gmail.com>
2025-10-05 03:13:40 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, request, render_template, send_file
+from PyPDF2 import PdfReader, PdfWriter
+
+import os, io, secrets
+from werkzeug.exceptions import RequestEntityTooLarge
+
+app = Flask(__name__)
+
+UPLOAD_FOLDER = "uploads"
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+@app.errorhandler(RequestEntityTooLarge)
+def largefile(e):
+    return "too large file,pls upload lower than 40mb",413  
+def encryptpdf(filestream,password):
+        pdf_reader=PdfReader(filestream)
+        pdf_writer=PdfWriter()
+        for page in pdf_reader.pages:
+            pdf_writer.add_page(page)
+        pdf_writer.encrypt(password)
+
+        output_stream=io.BytesIO()
+        pdf_writer.write(output_stream)
+        output_stream.seek(0)
+        return output_stream
+@app.route('/upload',methods=['POST'])
+def upload():
+    file=request.files.get("file")
+    password=request.form.get("password")
+
+    if not file or not password:
+        return "FILE AND PASSWORD MUST BE UPLOADED"
+    output_stream=encryptpdf(file.stream,password)
+    return send_file(output_stream,as_attachment=True,download_name=secrets.token_hex(4)+".pdf"
+                         ,mimetype="application/pdf")
+if __name__ == "__main__":
+    app.run()
